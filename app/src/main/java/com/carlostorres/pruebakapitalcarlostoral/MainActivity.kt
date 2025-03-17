2e5d739311d532754aae78ff235c444fd556beb8
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2025-03-16 19:18:07 -0600

    init config

---
+++ app/src/main/java/com/carlostorres/pruebakapitalcarlostoral/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.carlostorres.pruebakapitalcarlostoral
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.carlostorres.pruebakapitalcarlostoral.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
